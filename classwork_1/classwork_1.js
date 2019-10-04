1c35f71d881600a74112c85bcdcec6115480d04e
Zeeshan Ahmad Khalil <mk1411997@gmail.com>
2019-10-04 15:16:40 +0500

    adding class work 1 and 2

---
+++ classwork_1/classwork_1.js
@@ -0,0 +1,23 @@
+student={
+    name:"Zeeshan",
+    age:28,
+    main_work:function(){
+        console.log(this.name+" study daily")
+    }
+}
+student.main_work()
+class Student{
+    constructor(name,age){
+        this.name=name
+        this.age=age
+    }
+}
+stu1=new Student("Zeeshan",22)
+console.log(stu1)
+student.father_name="Zulfiqar"//adding new property in student
+console.log(student)
+student.display_age=function(){// adding function in student
+    console.log("The age is "+student.age)
+}
+student.display_age()
+Object.defineProperty(student,"hobby",{"marvel movies"})
